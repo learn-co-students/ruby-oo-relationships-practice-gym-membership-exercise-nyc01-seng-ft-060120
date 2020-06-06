95e9010e4ec58f41da8e83509bf4b1a6f6d790a3
mayalexandera <maya.blevins@gmail.com>
2020-06-06 19:37:40 -0400

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,33 @@
 class Lifter
   attr_reader :name, :lift_total
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def memberships
+    Membership.all.select {|membership| membership.lifter == self}
+  end
+
+  def gyms 
+    self.memberships.map{ |membership| membership.gym.name}
+  end
+
+  def self.average_lift
+    all_totals = @@all.inject(0){|sum, e| sum + e.lift_total}
+    all_totals / @@all.length
+  end
+
+  def total_cost
+    self.memberships.inject(0){|sum, e| sum + e.cost }
+  end
+
+  def new_membership(gym, membership_cost)
+    Membership.new(membership_cost, self, gym)
+  end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -2,8 +2,32 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
-# test code goes here
+def reload
+  load 'config/environment.rb'
+end
 
-binding.pry
 
-puts "Gains!"
+lifter1 = Lifter.new("Jim", 1000)
+lifter2 = Lifter.new("Annie", 4000)
+lifter3 = Lifter.new("Bjorn", 500)
+lifter4 = Lifter.new("Richard", 3000)
+lifter5 = Lifter.new("Sam", 1500)
+lifter6 = Lifter.new("Katrin", 3600)
+
+gym1 = Gym.new("Meatheads")
+gym2 = Gym.new("The Best Gym")
+gym3 = Gym.new("The Dirty Gym")
+gym4 = Gym.new("The Fancy Gym")
+
+membership1 = Membership.new(150, lifter1, gym1)
+membership2 = Membership.new(250, lifter1, gym4)
+membership3 = Membership.new(50, lifter2, gym2)
+membership4 = Membership.new(150, lifter1, gym1)
+membership5 = Membership.new(250, lifter6, gym4)
+membership6 = Membership.new(150, lifter3, gym1)
+membership7 = Membership.new(200, lifter4, gym3)
+membership8 = Membership.new(250, lifter5, gym4)
+membership9 = Membership.new(150, lifter5, gym2)
+
+
+Pry.start
