9aac7d27027ba50eded5bfcf2ebb9b37db97a60e
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-06-08 01:27:26 -0400

    Done.

--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,16 @@
 class Membership
-  attr_reader :cost
+  attr_reader :cost, :gym, :lifter
 
-  def initialize(cost)
+  @@all = []
+
+  def initialize(lifter,gym,cost)
     @cost = cost
+    @gym = gym
+    @lifter = lifter
+    @@all << self 
   end
+
+  def self.all
+    @@all
+  end 
 end
