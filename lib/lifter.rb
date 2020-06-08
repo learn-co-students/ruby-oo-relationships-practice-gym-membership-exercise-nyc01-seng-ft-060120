4ab1370b53ba58d2e2d60495a4ff521f9d64c6c0
maxcroy1 <max.croy1@gmail.com>
2020-06-08 16:11:48 -0400

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,37 @@
 class Lifter
+
   attr_reader :name, :lift_total
+
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def new_membership(cost, gym)
+    Membership.new(cost, self, gym)
+  end
+
+  def memberships
+    Membership.all.select{|membership| membership.lifter == self}
+  end
+
+  def cost_of_memberships
+    self.memberships.sum{|membership| membership.cost}
+  end
+
+  def gyms
+    self.memberships.map{|membership| membership.gym}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.average_lift_total
+    @@all.sum{|lifter| lifter.lift_total} / @@all.count
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,34 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+lifter_one = Lifter.new("Max", 2300)
+lifter_two = Lifter.new("Sam", 1250)
+lifter_three = Lifter.new("Amanda", 300)
+
+gym_one = Gym.new("Gym One")
+gym_two = Gym.new("Gym Two")
+gym_three = Gym.new("Gym Three")
+
+Lifter.all
+Lifter.average_lift_total
+Gym.all
+
+lifter_one.new_membership(80, gym_one)
+lifter_one.new_membership(120, gym_three)
+lifter_two.new_membership(45, gym_two)
+lifter_three.new_membership(45, gym_two)
+
+Membership.all
+
+lifter_one.memberships
+lifter_one.cost_of_memberships
+lifter_one.gyms
+
+gym_two.memberships
+gym_two.lifters
+gym_two.lifters_by_name
+gym_two.combined_lift_total
+
 
 binding.pry
 
