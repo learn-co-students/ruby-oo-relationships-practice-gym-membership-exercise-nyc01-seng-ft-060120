8421c0097de82528d858361014c08c6420875990
mstaso <mstaso1@gmail.com>
2020-06-08 13:31:14 -0400

    Done.

---
+++ lib/run.rb
@@ -0,0 +1,6 @@
+require 'pry'
+require_relative './lifter.rb'
+require_relative './gym.rb'
+require_relative './membership.rb'
+
+lifter1=Lifter.new("malcolm", 200)
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,16 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
-binding.pry
 
-puts "Gains!"
+
+lifter1=Lifter.new("malcolm", 400)
+lifter2=Lifter.new("john", 200)
+
+gym1=Gym.new("Chelsea Piers")
+gym2=Gym.new("equinox")
+
+membership1=Membership.new(100, lifter1, gym1)
+membership1premium=Membership.new(150, lifter1, gym1)
+membership2=Membership.new(200, lifter1, gym2)
+membership3=Membership.new(200, lifter2, gym2)
+binding.pry
